f93217f9281387d8b2d66d2233da57c1b77617e8
Daniel <daniel4671@hotmail.com>
2022-07-19 21:09:51 -0300

    respostas

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+
+let nome;
+let idade;
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+nome = (prompt("Qual é o seu nome?"))
+idade = (prompt("Qual é a sua idade?"))
+
+console.log(nome)
+console.log(idade)
+
+console.log("Meu nome é", nome, "e minha idade é", idade, "anos")
+
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+
+const PergA = "Você está usando uma roupa azul hoje?"
+const PergB = "Você é brasileiro?"
+const PergC = "Você é do RJ?"
+
+const RespA = prompt(PergA)
+const RespB = prompt(PergB)
+const RespC = prompt(PergC)
+
+console.log(PergA, RespA, "-", PergB, RespB, "-", PergC, RespC)
+
